01d6218fc4cc9a038d7c6ed0936b29cc350112aa
Ronnie Magatti <ronniemagatti@gmail.com>
2023-04-27 02:19:51 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# 🌟 Logger.nvim
+
+This plugin provides a logger class for logging messages via the `vim.notify` function in Neovim.
+It provides consistent logging and uses `vim.inspect` under the hood as well when possible to log tables.
+
+## 🚀 Usage
+
+To use the logger class, first require it in your Neovim Lua plugin:
+
+```lua
+local Logger = require("logger").new({ log_level = "debug", prefix = "my_prefix" })
+```
+
+You can optionally set the `log_level` and `prefix` using the setter functions:
+
+```lua
+logger:set_log_level("debug")
+logger:set_prefix("my_prefix")
+```
+
+Then, you can use the logging methods to log messages:
+
+```lua
+logger:debug("debug message")
+logger:info("info message")
+logger:warn("warning message")
+logger:error("error message")
+```
+
+e.g
+```lua
+-- Logging examples
+logger:debug("This is a debug message.") -- Logs "my_prefix DEBUG: This is a debug message." as a vim.notify with level DEBUG
+logger:info("This is an info message.") -- Logs "my_prefix INFO: This is an info message." as a vim.notify with level INFO
+logger:warn("This is a warning message.") -- Logs "my_prefix WARN: This is a warning message." as a vim.notify with level WARN
+logger:error("This is an error message.") -- Logs "my_prefix ERROR: This is an error message." as a vim.notify with level ERROR
+
+-- Logging tables
+logger:debug("something", { something = "foo", bar = "baz" })
+
+my_prefix DEBUG: something { something = "foo", bar = "baz" }
+```
+
+## 🔌 API
+
+### Constructor
+
+```lua
+Logger:new(obj_and_config)
+```
+
+Creates a new logger instance with an optional `obj_and_config` table. The table can contain the `log_level` and `prefix` properties to set the default log level and prefix for the logger.
+
+### Setters
+
+```lua
+Logger:set_log_level(level)
+```
+
+Sets the log level for the logger. `level` can be either a string or number, as described in `:h vim.log.levels`.
+
+```lua
+Logger:set_prefix(prefix)
+```
+
+Sets the prefix for the logger. The prefix is logged alongside the log level in each message.
+
+### Logging methods
+
+```lua
+Logger:debug(...)
+```
+
+Logs a debug message. This is the most verbose logging level, and will only log if the log level is set to "debug" or `vim.log.levels.DEBUG`.
+
+```lua
+Logger:info(...)
+```
+
+Logs an info message. This is the default logging level, and will log if the `log_level` is set to `"info"`, `"debug"`, or `vim.log.levels.DEBUG` or `vim.log.levels.INFO`.
+
+```lua
+Logger:warn(...)
+```
+
+Logs a warning message. This will log if the `log_level` is set to `"warn"`, `"info"`, `"debug"`, or `vim.log.levels.DEBUG` or `vim.log.levels.INFO` or `vim.log.levels.WARN`.
+
+```lua
+Logger:error(...)
+```
+
+Logs an error message. This will log if the `log_level` is set to `"error"`, `"warn"`, `"info"`, `"debug"`, or `vim.log.levels.DEBUG` or `vim.log.levels.INFO` or `vim.log.levels.WARN` or `vim.log.levels.ERROR`.
+
+## 📝 Notes
+
+This plugin uses `vim.notify` to log messages, so make sure that you have the necessary Neovim version and configuration to use this function.
---
+++ lua/logger/init.lua
@@ -0,0 +1,100 @@
+---Logger class for logging messages via vim.notify.
+---Usage: `local logger = require("logger").new({ log_level = "debug", prefix = "my_prefix" })`
+---Optionally set the `log_level` and `prefix` using the setter functions.
+---e.g. `logger:set_log_level("debug")` or `logger:set_prefix("my_prefix")`
+---@class Logger
+local Logger = {}
+
+---Function that handles vararg printing, so logs are consistent.
+---@vararg any
+local function to_print(...)
+	local args = { ... }
+	if #args == 1 and type(...) == "table" then
+		return vim.inspect(...)
+	else
+		local to_return = ""
+
+		for _, value in ipairs(args) do
+			to_return = vim.fn.join({ to_return, vim.inspect(value) }, " ")
+		end
+
+		return to_return
+	end
+end
+
+---Constructor for Logger class.
+---@param obj_and_config table Table containing the object and configuration for the logger.
+function Logger:new(obj_and_config)
+	obj_and_config = obj_and_config or {}
+	-- Set default log_level
+	self.log_level = vim.log.levels.INFO
+
+	self = vim.tbl_deep_extend("force", self, obj_and_config)
+	self.__index = function(_, index)
+		if type(self[index]) == "function" then
+			return function(...)
+				-- Make it so any call to logger with "." dot access for a function results in the syntactic sugar of ":" colon access
+				self[index](self, ...)
+			end
+		else
+			return self[index]
+		end
+	end
+
+	setmetatable(obj_and_config, self)
+
+	return obj_and_config
+end
+
+---Set the log level for the logger.
+---@param level string|number Either a string or number, see `:h vim.log.levels`
+function Logger:set_log_level(level)
+	self.log_level = level
+end
+
+---Set the prefix for the logger.
+---the prefix is logged alongside the log_level in each message.
+---@param prefix string
+function Logger:set_prefix(prefix)
+	self.prefix = prefix
+end
+
+---Log a debug message.
+---The most amount of logging, logs only if the log_level is set to "debug" or `vim.log.levels.DEBUG`
+---@vararg any
+function Logger:debug(...)
+	if self.log_level == "debug" or self.log_level == vim.log.levels.DEBUG then
+		vim.notify(vim.fn.join({ self.prefix .. " " .. "DEBUG:", to_print(...) }, " "), vim.log.levels.DEBUG)
+	end
+end
+
+---Log an info message.
+---The default level of logging, logs if the `log_level` is set to `"info"`, `"debug"`, or `vim.log.levels.DEBUG` or `vim.log.levels.INFO`
+---@vararg any
+function Logger:info(...)
+	local valid_values = { "info", "debug", vim.log.levels.DEBUG, vim.log.levels.INFO }
+
+	if vim.tbl_contains(valid_values, self.log_level) then
+		vim.notify(vim.fn.join({ self.prefix .. " " .. "INFO:", to_print(...) }, " "), vim.log.levels.INFO)
+	end
+end
+
+---Log a warning message.
+---Logs if the `log_level` is set to `"warn"`, `"info"`, `"debug"`, or `vim.log.levels.DEBUG` or `vim.log.levels.INFO` or `vim.log.levels.WARN`
+---@vararg any
+function Logger:warn(...)
+	local valid_values = { "info", "debug", "warn", vim.log.levels.DEBUG, vim.log.levels.INFO, vim.log.levels.WARN }
+
+	if vim.tbl_contains(valid_values, self.log_level) then
+		vim.notify(vim.fn.join({ self.prefix .. " " .. "WARN:", to_print(...) }, " "), vim.log.levels.WARN)
+	end
+end
+
+---Log an error message.
+---Logs if the `log_level` is set to `"error"`, `"warn"`, `"info"`, `"debug"`, or `vim.log.levels.DEBUG` or `vim.log.levels.INFO` or `vim.log.levels.WARN` or `vim.log.levels.ERROR`
+---@vararg any
+function Logger:error(...)
+	vim.notify(vim.fn.join({ self.prefix .. " " .. "ERROR:", to_print(...) }, " "), vim.log.levels.ERROR)
+end
+
+return Logger
